4fba9cbd8817673dfed88cd45aa93a111a3a7700
luisethyomi <142243350+luisethyomi@users.noreply.github.com>
2024-10-22 11:02:29 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,26 @@
+criaCartao(
+//categoria,
+'Programação'
+//pergunta,
+'O que é JavaScript?'
+//resposta,
+'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    //categoria,
+    'Programação'
+    //pergunta,
+    'O que é JavaScript?'
+    //resposta,
+    'O JavaScript é uma linguagem de programação'
+    )
+
+    criaCartao(
+        //categoria,
+        'Programação'
+        //pergunta,
+        'O que é JavaScript?'
+        //resposta,
+        'O JavaScript é uma linguagem de programação'
+        )
