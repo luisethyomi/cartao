13fdc443114b058f20a4aac808b6b9bc8b9497f7
luisethyomi <142243350+luisethyomi@users.noreply.github.com>
2024-10-29 11:01:23 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,26 +1,22 @@
 criaCartao(
-//categoria,
-'Programação'
-//pergunta,
-'O que é JavaScript?'
-//resposta,
-'O JavaScript é uma linguagem de programação'
+   'Programação',
+   'O que é JavaScript?',
+   'O JavaScript é uma linguagem de programação'
 )
 
 criaCartao(
-    //categoria,
-    'Programação'
-    //pergunta,
-    'O que é JavaScript?'
-    //resposta,
-    'O JavaScript é uma linguagem de programação'
-    )
+   'Programação',
+   'O que é JavaScript?',
+   'O JavaScript é uma linguagem de programação'
+)
+criaCartao(
+   'Programação',
+   'O que é JavaScript?',
+   'O JavaScript é uma linguagem de programação'
+)
 
-    criaCartao(
-        //categoria,
-        'Programação'
-        //pergunta,
-        'O que é JavaScript?'
-        //resposta,
-        'O JavaScript é uma linguagem de programação'
-        )
+criaCartao(
+    'Literatura',
+    'qual o nome da Saga de Arlen Fardos',
+    'o nome é Ciclo Das Trevas'
+)
