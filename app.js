0dff3616fa5df2e77f93814bfa896397133f9dbd
luisethyomi <142243350+luisethyomi@users.noreply.github.com>
2024-11-05 10:43:46 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,17 @@ let container = document.getElementById('container1')
 let cartao = document.createElement(article)
 cartao.className = 'cartao'
 
-
-
-
-
+  cartao.innerHTML = '
+  <div class="__conteudo">
+  <h3>${categoria}</h3>
+  <div class="cartao__conteudo__pergunta">
+     <p>${pergunta}</p>
+    </div>
+  <div class="cartao__conteudo__resposta">
+       <p>${resposta}</p>
+    </div>
+    </div>
+  '
+   container.appendChild(cartao)
+  
 }
